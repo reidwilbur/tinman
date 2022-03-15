df92bab2389e9f1646dcfd199ccc38f2388b1d85
Reid Wilbur <reid.wilbur@gmail.com>
2022-03-15 11:55:58 -0500

    Adding some state to script to get camevents and checkmtg working together

--- scripts/tinman-automator.py
+++ scripts/tinman-automator.py
@@ -7,16 +7,101 @@ import configparser
 import random
 from subprocess import Popen, PIPE
 from datetime import datetime
+import sys
 
-config = configparser.ConfigParser()
+class TinmanApi:
+    config_file_name = "tinman-automator.conf"
+    modes = ['digital_rain', 'sparkle', 'fire', 'static', 'kitt', 'nyancat']
+    check_mtg_days = [day for day in range(1,6)]
+    check_mtg_hours = [hour for hour in range(7, 16)]
 
-modes = ['digital_rain', 'sparkle', 'fire', 'static', 'kitt', 'nyancat']
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.getConfig()
 
-check_mtg_days = [day for day in range(1,6)]
-check_mtg_hours = [hour for hour in range(7, 16)]
+    def getConfig(self):
+        self.config.read(self.config_file_name)
+        return self.config
+
+    def writeConfig(self):
+        with open(self.config_file_name, "w") as file:
+            self.config.write(file)
+
+    def set_sleep(self, sleep):
+        ticker_url = self.config["urls"]["ticker_url"]
+        url = ticker_url + "sleep"
+        fields = {"sleep": "true" if (sleep) else "false"}
+        req = Request(url, urlencode(fields).encode())
+        with urlopen(req) as resp:
+            return resp.status == 200
+
+    def working_hour(self):
+        now = datetime.now()
+        return (now.isoweekday() in self.check_mtg_days) and (now.hour in self.check_mtg_hours)
+
+    def check_mtg(self):
+        if (self.working_hour()):
+            mode = self.get_mode()
+            can_write = not (mode.lower() == "ticker" and self.config["state"]["last_write"] == self.cam_events.__name__)
+            if can_write:
+                mtg_status_url = self.config["urls"]["mtg_status_url"]
+                with urlopen(mtg_status_url) as resp:
+                    if resp.status == 200:
+                        json_resp = json.loads(resp.read().decode())
+                        if json_resp["data"]["inMtg"]:
+                            self.in_mtg()
+                        else:
+                            self.random_mode()
+                self.config["state"]["last_write"] = sys._getframe().f_code.co_name
+                self.writeConfig()
+
+    def random_mode(self):
+        self.set_mode(random.choice(self.modes))
+
+    def in_mtg(self):
+        self.set_mode("ticker", msg="in meeting", color="0xffff00")
+
+    def get_mode(self):
+        ticker_url = self.config["urls"]["ticker_url"]
+        url = ticker_url + "mode"
+        with urlopen(url) as resp:
+            if resp.status == 200:
+                return resp.read().decode().strip()
+            else:
+                return ""
+
+    def set_mode(self, mode, msg=None, color=None, speed=None):
+        ticker_url = self.config["urls"]["ticker_url"]
+        url = ticker_url + "mode"
+        fields = {"mode": mode}
+        if (msg != None):
+            fields["msg"] = msg
+        if (color != None):
+            fields["clr"] = color
+        if (speed != None):
+            fields["speed"] = speed
+        req = Request(url, urlencode(fields).encode())
+        with urlopen(req) as resp:
+            return resp.status == 200
+
+    def cam_events(self):
+        log_cmd = ["log", "stream", "--predicate", 'eventMessage contains "Post event kCameraStream"']
+        with Popen(log_cmd, stdout=PIPE) as cam_events:
+            while cam_events.poll() == None:
+                cam_event = str(cam_events.stdout.readline())
+                self.getConfig()
+                if (self.working_hour()):
+                    if ("kCameraStreamStart" in cam_event):
+                        print("setting ticker to in meeting")
+                        self.in_mtg()
+                    elif ("kCameraStreamStop" in cam_event):
+                        print("setting ticker to random mode")
+                        self.random_mode()
+                self.config["state"]["last_write"] = sys._getframe().f_code.co_name
+                self.writeConfig()
 
 def main():
-    config.read("tinman-automator.conf")
+    api = TinmanApi()
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--inmtg", action="store_true", help="Set ticker to in meeting text scroll")
@@ -24,91 +109,26 @@ def main():
     group.add_argument("--clear", action="store_true", help="Clear text on ticker")
     group.add_argument("--camevents", action="store_true", help="Attach to log and stream for camera events")
     group.add_argument("--ticker", action="store", help="Set ticker with message", metavar='<text>')
-    group.add_argument("--mode", choices=modes, help="Set ticker mode. Allowed values are " + ", ".join(modes), metavar="<mode>")
+    group.add_argument("--mode", choices=api.modes, help="Set ticker mode. Allowed values are " + ", ".join(api.modes), metavar="<mode>")
     group.add_argument("--sleep", action=argparse.BooleanOptionalAction, help="Set sleep mode (disables display)")
     args = parser.parse_args()
     if args.inmtg:
-        in_mtg()
+        api.in_mtg()
     elif args.checkmtg:
-        check_mtg()
+        api.check_mtg()
     elif args.clear:
-        set_mode("ticker", msg="")
+        api.set_mode("ticker", msg="")
     elif args.camevents:
-        cam_events()
+        api.cam_events()
     elif args.mode != None:
-        set_mode(args.mode)
+        api.set_mode(args.mode)
     elif args.ticker != None:
-        set_mode("ticker", args.ticker)
+        api.set_mode("ticker", args.ticker)
     elif args.sleep != None:
-        set_sleep(args.sleep)
+        api.set_sleep(args.sleep)
     else:
         parser.print_usage()
         exit(1)
-
-def set_sleep(sleep):
-    ticker_url = config["DEFAULT"]["ticker_url"]
-    url = ticker_url + "sleep"
-    fields = {"sleep": "true" if (sleep) else "false"}
-    req = Request(url, urlencode(fields).encode())
-    with urlopen(req) as resp:
-        return resp.status == 200
-
-def working_hour():
-    now = datetime.now()
-    return (now.isoweekday() in check_mtg_days) and (now.hour in check_mtg_hours)
-
-def check_mtg():
-    if (working_hour()):
-        mode = get_mode()
-        if (mode.lower() != "ticker"):
-            mtg_status_url = config["DEFAULT"]["mtg_status_url"]
-            with urlopen(mtg_status_url) as resp:
-                if resp.status == 200:
-                    json_resp = json.loads(resp.read().decode())
-                    if json_resp["data"]["inMtg"]:
-                        in_mtg()
-                    else:
-                        random_mode()
-
-def random_mode():
-    set_mode(random.choice(modes))
-
-def in_mtg():
-    set_mode("ticker", msg="in meeting", color="0xffff00")
-
-def get_mode():
-    ticker_url = config["DEFAULT"]["ticker_url"]
-    url = ticker_url + "mode"
-    with urlopen(url) as resp:
-        if resp.status == 200:
-            return resp.read().decode().strip()
-        else:
-            return ""
-
-def set_mode(mode, msg=None, color=None, speed=None):
-    ticker_url = config["DEFAULT"]["ticker_url"]
-    url = ticker_url + "mode"
-    fields = {"mode": mode}
-    if (msg != None):
-        fields["msg"] = msg
-    if (color != None):
-        fields["clr"] = color
-    if (speed != None):
-        fields["speed"] = speed
-    req = Request(url, urlencode(fields).encode())
-    with urlopen(req) as resp:
-        return resp.status == 200
-
-def cam_events():
-    log_cmd = ["log", "stream", "--predicate", 'eventMessage contains "Post event kCameraStream"']
-    with Popen(log_cmd, stdout=PIPE) as cam_events:
-        while cam_events.poll() == None:
-            cam_event = str(cam_events.stdout.readline())
-            if (working_hour()):
-                if ("kCameraStreamStart" in cam_event):
-                    in_mtg()
-                elif ("kCameraStreamStop" in cam_event):
-                    random_mode()
 
 if __name__ == "__main__":
     main()
